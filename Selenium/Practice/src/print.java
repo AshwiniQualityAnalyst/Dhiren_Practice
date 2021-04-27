1e7bd469c855a13f2826ec6566de6c333fadf8dd
AshwiniQualityAnalyst <ashwini.qualityanalyst@gmail.com>
2021-04-27 23:38:29 +0530

    Fourth commit

--- Selenium/Practice/src/print.java
+++ Selenium/Practice/src/print.java
@@ -1,9 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class print {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\AutomationTools\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();       
+		driver.get("https://google.com");
+		driver.quit();
 	}
 
 }
